351dc5d01d69798be9a14c72d7d178685a8dd85a
Yee Lock <yeelockwong@gmail.com>
2026-01-06 18:01:14 +0800

    Refactor LoginSystem with current year and comments
    Updated the year constant and improved comments.

--- LoginSystem.java
+++ LoginSystem.java
@@ -1,21 +1,23 @@
-import java.util.Scanner;
+// A simple console-based login system for learning purposes.
+// This program simulates a basic login flow and does not implement real security or data storage.
+
+import java.util.Scanner; 
 import java.util.Random;
 public class LoginSystem{
-static final int YEAR=2025;
+static final int CURRENT_YEAR=2026;
 
     public static void main(String[] args){
         
         Scanner input=new Scanner(System.in);
         Random random=new Random();
-        boolean information=false;
         
         System.out.print("Please enter your name: ");
         String name=input.nextLine();
         System.out.println();
         System.out.print("Please enter your year born:");
         int year_born=input.nextInt();
-        input.nextLine();
-        int age=YEAR-year_born;
+        input.nextLine();  //Clear leftover newline after nextInt();
+        int age=CURRENT_YEAR-year_born;
         System.out.println("Your age is: "+age);
             
             if(age<18){
@@ -23,21 +25,21 @@ static final int YEAR=2025;
                 return;
             }
         
-        System.out.println();
+        System.out.println();  //new line (same as \n)
         System.out.print("Please enter your new password: ");
         String password=input.nextLine();
         System.out.println();
         System.out.print("Please enter your password again: ");
         String password2=input.nextLine();
         
-          while(!password.equals(password2)){
+          while(!password.equals(password2)){ //check until the second password same as first password
               System.out.println("The second password is not same as first password! Please type again. ");
               System.out.println();
               System.out.print("Please enter your password again: ");
               password2=input.nextLine();
           }
         
-        int passcode=random.nextInt(999999)+100000;
+        int passcode=random.nextInt(900000)+100000;
         System.out.println("Captcha: "+passcode);
         System.out.println("Please enter the same passcode above");
         int code=input.nextInt();
@@ -53,13 +55,10 @@ static final int YEAR=2025;
                 
             }
         
-        information=true;
-        
-            if(information==true){
-                System.out.println("Checking information....");
-                System.out.println("You are sucessfully login! ");
-                System.out.println("Welcome "+name+"!");
-            }
+            System.out.println("Checking information....");
+            System.out.println("You are sucessfully logged in! ");
+            System.out.println("Welcome "+name+"!");
         
     }
+
 }
